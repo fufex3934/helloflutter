867482b58dd0a573e9d755ff5a8e338314c228ec
fufex3934 <fufawakgari174@gmail.com>
2024-12-19 15:23:06 +0300

    custom widget

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,21 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MaterialApp(
-    home: Scaffold(
-      appBar: AppBar(
-        title: Text('Hello Flutter App'),
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Hello Flutter App'),
+        ),
+        body: Text('Hello Flutter'),
       ),
-      body: Text('Hello Flutter'),
-    ),
-  ));
+    );
+  }
 }
